30afd0fe87e301f230a726e0d07fde84e1ad1023
Simran Ahuja <125957972+simranahuja13@users.noreply.github.com>
2023-09-27 20:38:53 +0000

    discussion about conversion of Data Types & operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,6 +32,42 @@
 
 // --- conversion in string
 
-let someNumber = 33 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber); // string
+// let someNumber = 33 
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber); // string
+
+// ********************** OPERATIONS *************************
+
+
+// console.log(3**2);
+// console.log(2/3);
+
+// let str1 = "hi"
+// let str2 = " buddy"
+// console.log(str1+str2); // hi buddy
+
+// console.log("3" + 2); // 32
+// console.log( 3 + "2"); // 32
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); //32
+// console.log( (2+3) * 6 % 2);
+// console.log(1 + 2 + "2" + 2) ; // 322
+
+// console.log(+true); // 1
+// console.log(true+); // Error
+
+console.log(+""); //0
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 = 2+2
+// console.log(num3);
+
+let gameCounter = 100 
+
+console.log(gameCounter++); //100 (postfix) // usage ke abd increment hoti hai
+console.log(gameCounter); //101
+
+
+// console.log(++gameCounter);  // 101 (prefix) value increment pehle ho jati hai
+
+
