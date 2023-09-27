0236055434c4dc6ec2c8bfb1c69d30e9885319dd
Simran Ahuja <125957972+simranahuja13@users.noreply.github.com>
2023-09-27 19:32:28 +0000

    understood Let Const and Var

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,11 @@
+
 const accountId = 144533
 let accountEmail = "simran@gmail.com"
 var accountPassword = "12345"
 accountCity ="Gaya"
 let accountState; //not initialised ~~ undefined
 
+// Notes in Comments
 /*
 Prefer not to use-- 
 Var coz of issue in Block scope & Functional scope
