168b9c53a57fc25c7d0005375fd5e9e63f3d8bc5
Simran Ahuja <125957972+simranahuja13@users.noreply.github.com>
2023-09-27 18:58:52 +0000

    understood Var Let and Const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144533
+let accountEmail = "simran@gmail.com"
+var accountPassword = "12345"
+accountCity ="Gaya"
+let accountState; //not initialised ~~ undefined
+
+/*
+Prefer not to use-- 
+Var coz of issue in Block scope & Functional scope
+*/
+// accountId = 123 //not allowed
+ accountEmail ="s@gmail.com"
+accountPassword ="456"
+accountCity ="BodhGaya"
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
