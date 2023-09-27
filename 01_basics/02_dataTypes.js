470f71d9e75cbe79db5956541e55105261ecf14d
Simran Ahuja <125957972+simranahuja13@users.noreply.github.com>
2023-09-27 19:33:00 +0000

    learnt about Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" ; //treat all js code as newer version
+
+// alert(3+3)-- we are using Node.js not Browser that's why this code can't be run 
+console.log(3
+    +
+    3); //code readability should be high
+
+    let name = "hitesh"
+    let age = 34 
+    let isloggedIn = true 
+    let state;
+    let email = null 
+
+
+    // Primitive Data Types
+    // numbers => range -- 2 to power 53
+    // bigInt 
+    // string => ""
+    // boolean => true / false
+    // null => standalone value -- empty value
+    // undefined =>  means- not intialised , let state; 
+    // symbol => helps in finding uniqueness - used in React Components also used in Figma tool
+
+    // Non-Primitive Data Types
+    // Object
+    console.log(typeof  "simmy");
+    console.log(typeof  age);
+    console.log(typeof  null); // object
+    console.log(typeof  undefined); // undefined
