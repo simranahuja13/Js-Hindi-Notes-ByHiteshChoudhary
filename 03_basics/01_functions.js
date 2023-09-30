ec9f7b43872a35c783a4948f5c898652ed49605d
Simran Ahuja <125957972+simranahuja13@users.noreply.github.com>
2023-09-30 06:10:16 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,63 @@
+// reusable code written in a block
+
+// all code from line No.3 to 10 is Function Definition
+// function sayMyName(){
+//     console.log("P");
+//     console.log("I");
+//     console.log("Y");
+//     console.log("U");
+//     console.log("S");
+//     console.log("I");
+// }
+// sayMyName() // sayMyName =>reference  () =>execution
+
+// 
+
+// function addTwoNumbers(number1 ,number2 ){ // parameters
+//     console.log((number1+number2)); 
+//    }
+//    const res = addTwoNumbers(1,6) // arguments
+//    console.log(res); //undefined
+
+
+// function addTwoNumbers(number1 ,number2 ){ // parameters
+//      console.log("op");
+//      return number1+number2 ;  // use return when u want to store output in a variable and to 
+         //working of function => return stored in a variable or output can be shown through console.log
+
+//    }
+//    const res = addTwoNumbers(1,6) // arguments
+//    console.log(res); //7
+
+// function logInUserMess(userName) {
+//     // if(userName=== undefined){
+//         if(!userName){
+//         console.log("please enter a username")
+//         return
+//     }
+//     return `${userName} just logged in `
+    
+// }
+// console.log( logInUserMess("chirag")); // chirag just logged in 
+// console.log( logInUserMess("")); //  just logged in 
+// console.log( logInUserMess());
+
+// function calculateCartPrice (val1 , val2 , ...args){
+    
+// }
+// calculateCartPrice(200,300,300,490,560)
+// val1=>200
+// val2=>300
+// ...args=>300,490,560
+
+
+
+// const user={
+//  username:"hitesh",
+//   price: 399
+
+// }
+// const handleObject(anyobj){
+//     console.log(`Username is ${anyobj.username} and price is ${anyobj.price}`);
+//    }
+//    handleObject(user)
